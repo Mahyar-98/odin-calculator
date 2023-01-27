d55fa45a7b1825698d45d0cdcd4d7a7a8f4c3515
Mahyar <mahyar.erfanian1998@gmail.com>
2023-01-27 12:36:41 -0500

    Add index.html and link style.css and script.js and add basic math functions

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  const result = a + b;
+  return result;
+}
+
+function subtract(a, b) {
+  const result = a - b;
+  return result;
+}
+
+function multiply(a, b, ...arguments) {
+  let result = a * b;
+  for (let i = 0; i < arguments.length; i++) {
+    result *= arguments[i];
+  }
+  return result;
+}
+
+function divide(a, b) {
+  const result = a / b;
+  return result;
+}
